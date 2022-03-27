1e141c17a6799654462bfe280823edc122ad255a
TheMrCrowley <themrcrovvley@gmail.com>
2022-03-27 17:54:55 +0300

    feat: first complete app, need refactor

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import InfoPage from '../pages/InfoPage';
+import LoginPage from '../pages/LoginPage';
+import ProfilePage from '../pages/ProfilePage';
+import Posts from './Posts/Posts';
+import Calendar from './Calendar/Calendar';
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="info" element={<InfoPage />}>
+          <Route path=":page" element={<Posts />} />
+        </Route>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="calendar" element={<Calendar />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRouter;
